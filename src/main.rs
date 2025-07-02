0d74c6606ce562a25301186d69be99d342a0e077
Nate Stott <nate.stott@pm.me>
2025-07-01 22:14:10 -0400

    respond to multiple pings 1

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
-#![allow(unused_imports)]
-use std::io::Write;
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::io::Result;
 
-fn main() {
-    println!("Logs from your program will appear here!");
+fn handle_client(mut stream: TcpStream) -> Result<()> {
+    let mut buf = [0u8; 512];
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+    loop {
+        let n = stream.read(&mut buf)?;
+        if n == 0 {
+            break; // client closed
+        }
+        stream.write_all(b"+PONG\r\n")?;
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let ip = "127.0.0.1";
+    let port = "6379";
+    let address = format!("{ip}:{port}");
+    let listener = TcpListener::bind(&address)?;
+    println!("Listening on {address}...");
+
+    for incoming in listener.incoming() {
+        match incoming {
+            Ok(stream) => {
+                if let Err(e) = handle_client(stream) {
+                    eprintln!("client error: {}", e);
+                }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(e) => eprintln!("accept error: {}", e),
         }
     }
+    Ok(())
 }
